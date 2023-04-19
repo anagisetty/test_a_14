6292ef24a92445d256c561b94be29e9c788b72e8
dotnet-code <@dotnet-code>
2023-04-19 17:02:15 +0530

    commit from code202304190215

---
+++ test_a_14/Controllers/SecurityExchangeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using test_a_14.SecurityExchangeProcess;
+
+namespace test_a_14.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecurityExchangeController : ControllerBase
+    {
+        private readonly ISecurityExchangeService _securityExchangeService;
+
+        public SecurityExchangeController(ISecurityExchangeService securityExchangeService)
+        {
+            _securityExchangeService = securityExchangeService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<SecurityExchange>> GetSecurityExchangeData()
+        {
+            var result = _securityExchangeService.GetSecurityExchangeData();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateSecurityExchange(SecurityExchange securityExchange)
+        {
+            await _securityExchangeService.CreateSecurityExchange(securityExchange);
+            return Ok();
+        }
+    }
+}
